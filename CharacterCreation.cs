b553690a077d07530cd8c5ec57d5a2884db5ec31
Christopher Hall <christoperbhall@gmail.com>
2015-02-19 16:01:25 -0500

    Character Creation Script
    The Character Creation script, controls the set up of a character per
    location

---
+++ CharacterCreation.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CharacterCreation : MonoBehaviour {
+
+	public static bool characterCreated = false;
+	public static bool characterIsBeingCreated = true;
+	public static bool genderSelected = false;
+	public GameObject femaleResident;
+	public GameObject maleResident;
+
+	// Use this for initialization
+	void Start () {
+		characterCreated = GameSaveLoad.room1CharacterCreatedSaveLoad;
+		isFemale = GameSaveLoad.room1IsFemaleSaveLoad;
+		isMale = GameSaveLoad.room1IsMaleSaveLoad;
+		characterIsBeingCreated = true;
+		if (characterCreated == true) {
+			maleButtonBackground.enabled = false;
+			maleButtonText.enabled = false;
+			femaleButtonBackground.enabled = false;
+			femaleButtonText.enabled = false;
+
+			
+			characterCreationGenderSelectionBackground.enabled = false;
+			characterCreationGenderSelectionCancelButton.enabled = false;
+			characterCreationGenderSelectionCancelButtonText.enabled = false;
+			characterCreationGenderSelectionSelectAGenderText.enabled = false;
+			characterIsBeingCreated = false;
+			if (isFemale == true) {
+				IsFemale ();
+			}
+			else if (isMale == true) {
+				IsMale ();
+			}
+			else
+				Debug.Log ("No gender found in the character creation if/then statement");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void IsFemale()
+	{
+		isFemale = true;
+		maleButtonBackground.enabled = false;
+		maleButtonText.enabled = false;
+		femaleButtonBackground.enabled = false;
+		femaleButtonText.enabled = false;
+		Instantiate(femaleResident);
+		characterCreationGenderSelectionBackground.enabled = false;
+		characterCreationGenderSelectionCancelButton.enabled = false;
+		characterCreationGenderSelectionCancelButtonText.enabled = false;
+		characterCreationGenderSelectionSelectAGenderText.enabled = false;
+		genderSelected = true;
+	}
+	
+	public void IsMale()
+	{
+		isMale = true;
+		maleButtonBackground.enabled = false;
+		maleButtonText.enabled = false;
+		femaleButtonBackground.enabled = false;
+		femaleButtonText.enabled = false;
+		Instantiate(maleResident);
+		characterCreationGenderSelectionBackground.enabled = false;
+		characterCreationGenderSelectionCancelButton.enabled = false;
+		characterCreationGenderSelectionCancelButtonText.enabled = false;
+		characterCreationGenderSelectionSelectAGenderText.enabled = false;
+		genderSelected = true;
+	}
+}
